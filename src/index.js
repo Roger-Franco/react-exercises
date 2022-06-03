eb8508e0866dddca48ae69b106c6e1dd4ebadc12
Roger-Franco <rogerfrancogalt@gmail.com>
2022-06-03 11:00:20 -0400

    Múltiplos Componentes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import BomDia from './componentes/BomDia'
+import Multi, { BoaNoite, BoaTarde } from './componentes/Multiplos'
 
-ReactDOM.render(<BomDia nome='Roger' />, document.getElementById('root'))
+ReactDOM.render(
+  <div>
+    <BomDia nome='Roger' />
+    <Multi.BoaNoite nome='Rogerrr' />
+    <BoaTarde nome='Roger' />
+  </div>
+  , document.getElementById('root'))
